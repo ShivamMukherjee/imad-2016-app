6ea3f9e392d7391d8ae5fb1932415ac25632e9b0
ShivamMukherjee <mshivam98@gmail.com>
2016-10-12 23:08:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,5 +6,5 @@ document.querySelector(".text-big").innerHTML = "Hi."
 // now it's Madi the dino
 var madi = document.querySelector(".img-medium");
 madi.onmouseover = function() {
-    madi.style.marginLeft = "100px";
+    madi.style.marginLeft += "100px";
 };
