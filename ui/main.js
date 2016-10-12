d8b987b3aff41dc523dd915ee96ef7e4df22b814
ShivamMukherjee <mshivam98@gmail.com>
2016-10-12 23:07:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
 console.log('Loaded!');
 
-document.querySelector(".text-big").innerHTML = "Dangu kothakar."
+// I changed the main text
+document.querySelector(".text-big").innerHTML = "Hi."
+
+// now it's Madi the dino
+var madi = document.querySelector(".img-medium");
+madi.onhover = function() {
+    madi.style.marginLeft = "100px";
+};
